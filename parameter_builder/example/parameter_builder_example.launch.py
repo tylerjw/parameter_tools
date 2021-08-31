8185a66a2e1577390b5aac5efbbbfc88e2bb5b99
Tyler Weaver <tylerjw@gmail.com>
2021-08-30 20:25:01 -0600

    Add pre-commit and github actions

--- parameter_builder/example/parameter_builder_example.launch.py
+++ parameter_builder/example/parameter_builder_example.launch.py
@@ -45,7 +45,9 @@ def generate_launch_description():
                     .file_parameter(
                         "parameter_file", "config/parameter_file"
                     )  # Or /absolute/path/to/file
-                    .yaml(file_path="config/parameters.yaml")  # Or /absolute/path/to/file
+                    .yaml(
+                        file_path="config/parameters.yaml"
+                    )  # Or /absolute/path/to/file
                     .xacro_parameter(
                         parameter_name="my_robot",
                         file_path="config/parameter.xacro",  # Or /absolute/path/to/file
